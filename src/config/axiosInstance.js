1a749fae82e0b17210b0ed1ff243a8c43905a710
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-09-26 09:41:09 -0300

    feat: axiosInstance

---
+++ src/config/axiosInstance.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const ipBackEnd = {
+    localhost: "localhost",
+}
+
+const axiosInstance = axios.create({
+    baseURL: `https://${ipBackEnd.localhost}:8080`,
+    timeout: 3000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+    const tokenUsuario = sessionStorage.getItem('token');
+
+    if (tokenUsuario) {
+        config.headers['authorization'] = `Bearer ${tokenUsuario}`;
+    }
+
+    return config;
+});
+
+export default axiosInstance;
