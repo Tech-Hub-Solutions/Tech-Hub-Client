56db690710f6ec46c012c5896a5288f3f72337a5
luizfiller <luiz.fsilva@sptech.school>
2023-09-26 19:20:54 -0300

    feat: componentização de buttons no header e 1ª section

---
+++ src/componentes/shared/BlueButton/BlueBackgroundButton.jsx
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import PropTypes from "prop-types";
+
+const BlueStyledButton = styled(Button)({
+  fontFamily: "Plus Jakarta Sans, sans serif",
+  textTransform: "none",
+  fontStyle: "normal",
+  fontSize: "16px",
+  padding: "10px 16px",
+  borderRadius: "6px",
+  fontWeight: "600",
+  backgroundColor: "#0F9EEA",
+  color: "#fdfdfd",
+  lineHeight: "1.3",
+});
+
+BlueBackgroundButton.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+function BlueBackgroundButton({ children }) {
+  return <BlueStyledButton variant="contained">{children}</BlueStyledButton>;
+}
+
+export default BlueBackgroundButton;
