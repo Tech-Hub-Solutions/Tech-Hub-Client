b78bbb826bfc671a437b316491f7eea84f58c07b
leodanave <leonardo.nave@sptech.school>
2023-09-26 15:29:49 -0300

    feat: first section done

--- src/componentes/Institucional/primeiraSecao/PrimeiraSecao.jsx
+++ src/componentes/Institucional/primeiraSecao/PrimeiraSecao.jsx
@@ -6,7 +6,12 @@ const PrimeiraSecao = () => {
     <div className="first__section">
       <div className="container__section">
         <div className="container__left-part">
-            <h1>Dficuldade em encontrar <span>talentos?</span></h1>
+          <h1>Dificuldade em <br />encontrar <span>talentos</span>?</h1>
+          <p>Unindo talento e necessidade: o ponto de encontro para desenvolvedores freelancers e empresas!</p>
+          <div className='container__buttons'>
+            <button className='button__left'>Quero ser um freelancer</button>
+            <button className='button__right'>Explorar talentos</button>
+          </div>
         </div>
         <div className="container__right-part">
           <img src={RhWoman} alt="" />
