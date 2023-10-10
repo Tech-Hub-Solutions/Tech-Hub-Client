d391ab05edb734cb1af8f50f46b8ac3ff2d85207
leodanave <leonardo.nave@sptech.school>
2023-10-10 11:34:16 -0300

    chore: estruturação das folders de perfil usuário

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Institucional from './pages/institucional/Institucional'
+import PerfilUsuario from './pages/perfilUsuario/PerfilUsuario';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Institucional />} />
+          <Route path="/perfil" element={<PerfilUsuario />} />
         </Routes>
       </Router>
     </>
