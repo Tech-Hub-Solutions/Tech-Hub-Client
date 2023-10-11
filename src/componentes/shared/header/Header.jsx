8beb1591414f39207aaa95f21f2fa5b52d60df6b
leodanave <leonardo.nave@sptech.school>
2023-10-11 11:49:48 -0300

    refactor: header do sistema

--- src/componentes/shared/header/Header.jsx
+++ src/componentes/shared/header/Header.jsx
@@ -1,17 +1,20 @@
 import styles from './header.module.css'
 import React from "react";
 import TechHubImg from "../../../assets/images/tech-hub-logo-text.svg"
-import IconeFavoritosImg from "../../../assets/images/IconeFavoritos.svg"
-import IconeMensagensImg from "../../../assets/images/IconeMensagens.svg"
+import AccountMenu from './materialComponents/AccountMenu';
+import { Link } from 'react-router-dom';
+import MessageOutlinedIcon from '@mui/icons-material/MessageOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 const Header = () => {
+
     return (
         <header className={styles['header']}>
             <div className={styles['content']}>
                 <div className={styles['logo__tech-hub']}>
-                    <a href="#">
+                    <Link to={'/'}>
                         <img src={TechHubImg} alt="Logo Tech Hub" />
-                    </a>
+                    </Link>
                 </div>
                 <div className={styles['right__content']}>
                     <ul>
@@ -26,13 +29,13 @@ const Header = () => {
                             </li>
                         </a>
                     </ul>
-                    <a href="">
-                        <img className={styles['icone__header']} src={IconeFavoritosImg} alt="Ícone de favoritos" />
-                    </a>
-                    <a href="">
-                        <img className={styles['icone__header']} src={IconeMensagensImg} alt="Ícone de mensagens" />
-                    </a>
-                    <img className={styles['foto__perfil']} src='' alt="Foto de perfil do usuário" />
+                    <Link to={'/'}>
+                        <FavoriteBorderOutlinedIcon className={styles['icone__header']} sx={{ fontSize: 26 }} />
+                    </Link>
+                    <Link to={'/'}>
+                        <MessageOutlinedIcon className={styles['icone__header']} sx={{ fontSize: 26 }} />
+                    </Link>
+                    <AccountMenu />
                 </div>
             </div>
         </header>
