f6adb1efdde672c3d7b9713f30aa61bb62a51114
luizfiller <luiz.fsilva@sptech.school>
2023-10-13 23:16:36 -0300

    feat[WIP]: add formulário de cadastro freela + emrpesa

--- src/componentes/modais/cadastro/CadastroModal.jsx
+++ src/componentes/modais/cadastro/CadastroModal.jsx
@@ -12,6 +12,10 @@ import CadastroFreelancerImage from "../../../assets/images/CadastroFreelancer.s
 import GoogleVetor from "../../../assets/images/GoogleVetor.svg";
 import Divider from "@mui/material/Divider";
 
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
 function CadastroModal({ user, isCadastroModalOpen, setIsCadastroModalOpen }) {
   const stylesCSS = {
     dialogContainer: {
@@ -29,7 +33,7 @@ function CadastroModal({ user, isCadastroModalOpen, setIsCadastroModalOpen }) {
       alignItems: "center",
       flexDirection: "column",
       overflow: "hidden",
-      gap: "56px",
+      gap: "16px",
       padding: 0,
     },
     dialogTitle: {
@@ -64,6 +68,39 @@ function CadastroModal({ user, isCadastroModalOpen, setIsCadastroModalOpen }) {
       padding: "18px 125px",
       marginTop: "48px",
     },
+  };
+
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
+
+  const [senha, setSenha] = useState("");
+  const [senhaError, setSenhaError] = useState(false);
+
+  const [nome, setNome] = useState("");
+  const [nomeError, setNomeError] = useState(false);
+
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+
+  const [documento, setDocumento] = useState("");
+  const [documentoError, setDocumentoError] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setNomeError(false);
+    setPasswordError(false);
+
+    if (nome == "") {
+      setNomeError(true);
+    }
+    if (password == "") {
+      setPasswordError(true);
+    }
+
+    if (nome && password) {
+      console.log(nome, password);
+    }
   };
 
   const handleClose = () => {
@@ -102,7 +139,7 @@ function CadastroModal({ user, isCadastroModalOpen, setIsCadastroModalOpen }) {
             />
           </div>
 
-          <div className={styles["form-container"]}>
+          <div className={styles["form__container"]}>
             <DialogTitle sx={stylesCSS.dialogTitle}>{"Cadastro"}</DialogTitle>
             <DialogContent sx={stylesCSS.dialogContent}>
               <Button variant="outlined" sx={stylesCSS.buttonGoogle}>
@@ -116,20 +153,86 @@ function CadastroModal({ user, isCadastroModalOpen, setIsCadastroModalOpen }) {
 
               <Divider sx={stylesCSS.customDivider}>OU</Divider>
 
-              {/* TODO - Adicionar os formulários aqui */}
-            </DialogContent>
+              <Grid container rowSpacing={1}>
+                <form autoComplete="off" onSubmit={handleSubmit} >
+                  <Grid item>
+                    <TextField
+                      label="Nome completo"
+                      onChange={(e) => setNome(e.target.value)}
+                      required
+                      variant="outlined"
+                      color="primary"
+                      type="text"
+                      sx={{ mb: 3 }}
+                      value={nome}
+                      error={nomeError}
+                      fullWidth
+                    />
+                  </Grid>
 
-            <BlueBackgroundButton
-              onClick={redictToBuscar}
-              style={stylesCSS.blueButton}
-            >
-              Cadastre-se
-            </BlueBackgroundButton>
+                  <Grid item>
+                    <TextField
+                      label="CPF"
+                      onChange={(e) => setDocumento(e.target.value)}
+                      required
+                      variant="outlined"
+                      color="primary"
+                      type="number"
+                      sx={{ mb: 3 }}
+                      value={documento}
+                      error={documentoError}
+                      fullWidth
+                    />
+                  </Grid>
+
+                  <Grid item>
+                    <TextField
+                      label="E-mail"
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      variant="outlined"
+                      color="primary"
+                      type="email"
+                      sx={{ mb: 3 }}
+                      value={email}
+                      error={emailError}
+                      fullWidth
+                    />
+                  </Grid>
+
+                  <Grid item>
+                    <TextField
+                      label="Senha"
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                      variant="outlined"
+                      color="primary"
+                      type="password"
+                      value={password}
+                      error={passwordError}
+                      sx={{ mb: 3 }}
+                      fullWidth
+                    />
+                  </Grid>
+
+                  <BlueBackgroundButton
+                    onClick={redictToBuscar}
+                    style={stylesCSS.blueButton}
+                    type="submit"
+                  >
+                    Cadastre-se
+                  </BlueBackgroundButton>
+                </form>
+              </Grid>
+            </DialogContent>
 
             <div className={styles["possui-conta"]}>
               <p>
                 Já tem conta?
-                <span className={styles["link-login"]}> Acesse aqui.</span>
+                <Link to="/busca-talentos" className={styles["link-login"]}>
+                  Acesse aqui.
+                </Link>
+                {/* TODO - pensar no path do <Link to > */}
               </p>
             </div>
           </div>
