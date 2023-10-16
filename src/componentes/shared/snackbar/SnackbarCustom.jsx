4e7b77dbe146936d989031f5be3c64c8ef28cb3d
luizfiller <luiz.fsilva@sptech.school>
2023-10-15 23:05:00 -0300

    feat: add useEffect de setTimeout p/ fechar snackbar

--- src/componentes/shared/snackbar/SnackbarCustom.jsx
+++ src/componentes/shared/snackbar/SnackbarCustom.jsx
@@ -3,12 +3,17 @@ import React from "react";
 import { Alert, Snackbar } from "@mui/material";
 
 function SnackbarCustom({ snackbarOpen, setSnackbarOpen, severity, message }) {
-  // TODO FIX - autoHideDuration não está funcionando no componente pai
+  React.useEffect(() => {
+    if (snackbarOpen) {
+      setTimeout(() => {
+        setSnackbarOpen(false);
+      }, 5000);
+    }
+  }, [snackbarOpen]);
 
   return (
     <Snackbar
       open={snackbarOpen}
-      autoHideDuration={5000}
       anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
       sx={{
         display: "flex",
