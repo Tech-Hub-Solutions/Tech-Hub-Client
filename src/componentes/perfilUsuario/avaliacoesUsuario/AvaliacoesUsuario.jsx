2c3ba44ef9ad6289530288dff39ddf8b88881e7c
leodanave <leonardo.nave@sptech.school>
2023-10-16 12:58:26 -0300

    feature: componente avaliações usuário

---
+++ src/componentes/perfilUsuario/avaliacoesUsuario/AvaliacoesUsuario.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./avaliacoesUsuario.module.css"
+
+const AvaliacoesUsuario = () => {
+    return ( 
+        <div className={styles['']}>
+
+        </div>
+     );
+}
+ 
+export default AvaliacoesUsuario;
