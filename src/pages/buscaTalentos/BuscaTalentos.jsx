bf4141b56e416396e6e3a330b08af241729a54bb
luizfiller <luiz.fsilva@sptech.school>
2023-10-21 15:17:45 -0300

    feat[WIP]: add estrutura de componente BuscaTalentos

---
+++ src/pages/buscaTalentos/BuscaTalentos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./BuscaTalentos.module.css";
+import Header from "../../componentes/shared/header/Header";
+
+const BuscaTalentos = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default BuscaTalentos;
