b13bcf6d910168fcbdafb7e024ebc1c12f0462cd
luizfiller <luiz.fsilva@sptech.school>
2023-11-07 18:12:46 -0300

    revert: favorito (erro no merge)

--- src/pages/favoritos/Favoritos.jsx
+++ src/pages/favoritos/Favoritos.jsx
@@ -1,220 +1,104 @@
-import React, { useState } from "react";
-import styles from "./compararUsuarios.module.css";
-import ArrowDropDownCircleOutlinedIcon from "@mui/icons-material/ArrowDropDownCircleOutlined";
-import CheckIcon from "@mui/icons-material/Check";
-import ClearIcon from "@mui/icons-material/Clear";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import CloseIcon from "@mui/icons-material/Close";
-import { Avatar, Button } from "@mui/material";
+import React from "react";
+import Header from "../../componentes/shared/header/Header";
+import styles from "./favoritos.module.css";
+import CardPerfil from "../../componentes/shared/cardPerfil/CardPerfil";
+import SelectOrdernar from "../../componentes/shared/SelectOrdernar";
+import axiosInstance from "../../config/axiosInstance";
 
-const CompararUsuarios = (props) => {
-  const {
-    usuariosSelecionados,
-    setUsuariosSelecionados,
-    showComparacao,
-    setComparacao,
-    conjuntoSkill,
-  } = props;
-  const [openDropdowns, setOpenDropdowns] = useState([]);
+import CompararUsuarios from "../../componentes/favoritos/CompararUsuarios";
 
-  const toggleDropdown = (tecnologia) => {
-    if (openDropdowns.some((dropdown) => dropdown === tecnologia)) {
-      setOpenDropdowns(
-        openDropdowns.filter((dropdown) => dropdown !== tecnologia)
-      );
-    } else {
-      setOpenDropdowns([...openDropdowns, tecnologia]);
-    }
-  };
+const Favoritos = () => {
+  const [valueOrdenar, setValueOrdenar] = React.useState("");
+  const [usuarios, setUsuarios] = React.useState([]);
+  const [usuariosSelecionados, setUsuariosSelecionados] = React.useState([]);
+  const [showComparacao, setComparacao] = React.useState(false);
+  const [conjuntoSkill, setConjuntoSkill] = React.useState([]);
+
+  React.useEffect(() => {
+    axiosInstance
+      .get("usuarios/favoritos")
+      .then((response) => {
+        if (response.status == 200) {
+          const responseUsuarios = response.data.content;
+          const conjuntoSkillResponse = [];
+
+          const responseMapeada = responseUsuarios.map((usuario) => {
+            usuario?.flags?.forEach((flag) => {
+              const area = conjuntoSkillResponse.find(
+                (area) => area.area == flag.area
+              );
+
+              if (!area) {
+                conjuntoSkillResponse.push({
+                  area: flag.area,
+                  tecnologias: [],
+                });
+              }
+
+              const tecnologia = area?.tecnologias.find(
+                (tecnologia) => tecnologia == flag.nome
+              );
+              if (!tecnologia) {
+                area?.tecnologias.push(flag.nome);
+              }
+            });
+
+            return {
+              ...usuario,
+              isFavorito: true,
+            };
+          });
+
+          setConjuntoSkill(conjuntoSkillResponse);
+          setUsuarios(responseMapeada);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
-    <div
-      className={`${styles["container"]} ${styles["comparacao"]} ${
-        showComparacao
-          ? styles["comparacao-active"]
-          : styles["comparacao-desativada"]
-      } `}
-    >
-      <div className={styles["comparacao__titulo"]}>
-        <h1>Comparar</h1>
-        <Button
-          onClick={() => {
-            setComparacao((prev) => !prev), setOpenDropdowns([]);
-          }}
-          sx={{
-            borderRadius: "50%",
-            width: "2.5rem",
-            height: "2.5rem",
-            minWidth: "2.5rem",
-            minHeight: "2.5rem",
-          }}
-        >
-          <ArrowDropDownCircleOutlinedIcon
-            sx={{
-              color: "var(--color-azul)",
-              fontSize: "2rem",
-              transition: "transform 0.3s ease-in-out",
-              transform: showComparacao ? "rotate(180deg)" : "rotate(0deg)",
-            }}
-          />
-        </Button>
-      </div>
-      <div className={styles["comparacao__content"]}>
-        <div className={styles["comparacao__content__header"]}>
-          {usuariosSelecionados.map((usuario) => (
-            <div
-              key={`content__header__${usuario.id}`}
-              className={styles["comparacao__content__header__item"]}
-            >
-              <Avatar
-                key={`content__header__${usuario.id}`}
-                alt={usuario.nome}
-                variant="square"
-                src={usuario.urlFotoPerfil}
-                sx={{ width: "10rem", height: "8rem" }}
-              >
-                {usuario.nome[0]}
-              </Avatar>
-              <div
-                className={styles["comparacao__content__header__item__info"]}
-              >
-                <p>{usuario.nome}</p>
+    <>
+      <Header />
+      <div className={styles["container"]}>
+        <div className={styles["content"]}>
+          <h1 className={styles["favoritos__titulo"]}>Favoritos</h1>
+          <div className={styles["favoritos__header"]}>
+            <h2 className={styles["favoritos__quantidade-desenvolvedores"]}>
+              {usuarios.length} desenvolvedores na lista de favoritos
+            </h2>
+            <SelectOrdernar
+              valueOrdenar={valueOrdenar}
+              setValueOrdenar={setValueOrdenar}
+            />
+          </div>
 
-                <Button
-                  onClick={() => {
-                    setUsuariosSelecionados(
-                      usuariosSelecionados.filter(
-                        (user) => user.id !== usuario.id
-                      )
-                    );
-                    if (usuariosSelecionados.length <= 2) {
-                      setComparacao(false);
-                    }
-                  }}
-                  sx={{
-                    borderRadius: "50%",
-                    width: "1.5rem",
-                    height: "1.5rem",
-                    minWidth: "1.5rem",
-                    minHeight: "1.5rem",
-                    color: "#FF7474",
-                  }}
-                >
-                  <CloseIcon sx={{ fontSize: "1.5rem", cursor: "pointer" }} />
-                </Button>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className={styles["comparacao__content__areas"]}>
-          {conjuntoSkill
-            .filter((skillSet) =>
-              usuariosSelecionados.some((usuario) =>
-                usuario.flags.some((flag) => flag.area === skillSet.area)
-              )
-            )
-            ?.map((conjunto) => (
-              <div
-                key={`comparacao__content__areas__${conjunto.area}`}
-                className={styles["comparacao__content__areas__area"]}
-              >
-                <div
-                  className={styles["comparacao__content__areas__area__titulo"]}
-                >
-                  <h3>{conjunto.area}</h3>
-                  <Button
-                    onClick={() => toggleDropdown(conjunto.area)}
-                    sx={{
-                      borderRadius: "50%",
-                      width: "1.5rem",
-                      height: "1.5rem",
-                      minWidth: "1.5rem",
-                      minHeight: "1.5rem",
-                    }}
-                  >
-                    <KeyboardArrowDownIcon
-                      sx={{
-                        fontSize: "2rem",
-                        color: "var(--color-azul)",
-                        transform: openDropdowns.some(
-                          (dropdown) => dropdown === conjunto.area
-                        )
-                          ? "rotate(180deg)"
-                          : "rotate(0deg)",
-                        transition: "transform 0.3s ease-in-out",
-                        cursor: "pointer",
-                      }}
-                    />
-                  </Button>
-                </div>
-
-                <div
-                  className={`${
-                    styles["comparacao__content__areas__area__dropdown"]
-                  }
-                                        ${
-                                          openDropdowns.some(
-                                            (dropdown) =>
-                                              dropdown === conjunto.area
-                                          )
-                                            ? styles["opened-dropdown"]
-                                            : styles["closed-dropdown"]
-                                        }`}
-                >
-                  {conjunto?.tecnologias
-                    .filter((tecnologia) =>
-                      usuariosSelecionados.some((usuario) =>
-                        usuario.flags.some((flag) => flag.nome == tecnologia)
-                      )
-                    )
-                    .map((tecnologia) => (
-                      <div
-                        key={`tecnologias__row__${tecnologia}`}
-                        className={
-                          styles[
-                            "comparacao__content__areas__area__tecnologias"
-                          ]
-                        }
-                      >
-                        <p
-                          className={
-                            styles[
-                              "comparacao__content__areas__area__tecnologias--nome"
-                            ]
-                          }
-                        >
-                          {tecnologia}
-                        </p>
-                        <div
-                          className={
-                            styles[
-                              "comparacao__content__areas__area__tecnologias--content"
-                            ]
-                          }
-                        >
-                          {usuariosSelecionados.map((usuario) => (
-                            <p
-                              key={`tecnologias__row__${tecnologia}__${usuario.id}`}
-                            >
-                              {usuario.flags.find(
-                                (flag) => flag.nome == tecnologia
-                              ) ? (
-                                <CheckIcon sx={{ color: "#47AF64" }} />
-                              ) : (
-                                <ClearIcon sx={{ color: "#FF7474" }} />
-                              )}
-                            </p>
-                          ))}
-                        </div>
-                      </div>
-                    ))}
-                </div>
-              </div>
+          <div className={styles["usuarios"]}>
+            {usuarios.map((usuario) => (
+              <CardPerfil
+                key={usuario.id}
+                usuario={usuario}
+                isTelaFavoritos={true}
+                setUsuarios={setUsuarios}
+                usuariosSelecionados={usuariosSelecionados}
+                setUsuariosSelecionados={setUsuariosSelecionados}
+              />
             ))}
+          </div>
         </div>
       </div>
-    </div>
+      {usuariosSelecionados.length > 1 && (
+        <CompararUsuarios
+          usuariosSelecionados={usuariosSelecionados}
+          setUsuariosSelecionados={setUsuariosSelecionados}
+          conjuntoSkill={conjuntoSkill}
+          showComparacao={showComparacao}
+          setComparacao={setComparacao}
+        />
+      )}
+    </>
   );
 };
 
-export default CompararUsuarios;
+export default Favoritos;
