ce3370fdbea71dcd7a49d51f79e5f53bdd90d211
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-11-07 21:44:11 -0300

    fix: correção de merge de branch favoritos

--- src/pages/favoritos/Favoritos.jsx
+++ src/pages/favoritos/Favoritos.jsx
@@ -1,104 +1,143 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "../../componentes/shared/header/Header";
-import styles from "./favoritos.module.css";
+import styles from "./Favoritos.module.css";
 import CardPerfil from "../../componentes/shared/cardPerfil/CardPerfil";
 import SelectOrdernar from "../../componentes/shared/SelectOrdernar";
-import axiosInstance from "../../config/axiosInstance";
+import axiosInstance from '../../config/axiosInstance'
 
-import CompararUsuarios from "../../componentes/favoritos/CompararUsuarios";
+import CompararUsuarios from "../../componentes/favoritos/compararUsuarios";
+import { Button } from "@mui/material";
+import CardPerfilSketon from "../../componentes/shared/cardPerfil/CardPerfilSkeleton";
 
 const Favoritos = () => {
-  const [valueOrdenar, setValueOrdenar] = React.useState("");
-  const [usuarios, setUsuarios] = React.useState([]);
-  const [usuariosSelecionados, setUsuariosSelecionados] = React.useState([]);
-  const [showComparacao, setComparacao] = React.useState(false);
-  const [conjuntoSkill, setConjuntoSkill] = React.useState([]);
+    const [valueOrdenar, setValueOrdenar] = React.useState("");
+    const [usuarios, setUsuarios] = React.useState([]);
+    const totalUsuarios = useRef(0);
+    const [usuariosSelecionados, setUsuariosSelecionados] = React.useState([]);
+    const [showComparacao, setComparacao] = React.useState(false);
+    const [conjuntoSkill, setConjuntoSkill] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(false);
 
-  React.useEffect(() => {
-    axiosInstance
-      .get("usuarios/favoritos")
-      .then((response) => {
-        if (response.status == 200) {
-          const responseUsuarios = response.data.content;
-          const conjuntoSkillResponse = [];
+    const page = useRef(0);
 
-          const responseMapeada = responseUsuarios.map((usuario) => {
-            usuario?.flags?.forEach((flag) => {
-              const area = conjuntoSkillResponse.find(
-                (area) => area.area == flag.area
-              );
+    React.useEffect(() => {
+        page.current = 0;
+        carregarUsuarios();
+    }, [valueOrdenar]);
 
-              if (!area) {
-                conjuntoSkillResponse.push({
-                  area: flag.area,
-                  tecnologias: [],
-                });
-              }
+    const handleVerMais = () => {
+        page.current++;
+        carregarUsuarios();
+    }
 
-              const tecnologia = area?.tecnologias.find(
-                (tecnologia) => tecnologia == flag.nome
-              );
-              if (!tecnologia) {
-                area?.tecnologias.push(flag.nome);
-              }
+    const carregarUsuarios = async () => {
+
+        setIsLoading(true);
+
+        await axiosInstance.get(`usuarios/favoritos?page=${page.current}&size=10&ordem=${valueOrdenar}`)
+            .then((response) => {
+                if (response.status == 200) {
+                    const responseUsuarios = response.data.content;
+                    const conjuntoSkillResponse = conjuntoSkill;
+
+                    if (totalUsuarios.current == 0) {
+                        totalUsuarios.current = response.data.totalElements;
+                    }
+
+                    const responseMapeada = responseUsuarios.map((usuario) => {
+
+                        usuario?.flags?.forEach(flag => {
+                            const area = conjuntoSkillResponse.find(area => area.area == flag.area);
+
+                            if (!area) {
+                                conjuntoSkillResponse.push({ area: flag.area, tecnologias: [] });
+                            }
+
+                            const tecnologia = area?.tecnologias.find(tecnologia => tecnologia == flag.nome);
+                            if (!tecnologia) {
+                                area?.tecnologias.push(flag.nome);
+                            }
+                        });
+
+                        return {
+                            ...usuario,
+                            isFavorito: true,
+                        };
+                    });
+
+                    setConjuntoSkill(conjuntoSkillResponse);
+                    setUsuarios(() => {
+                        if (page.current == 0) {
+                            return responseMapeada;
+                        }
+                        else {
+                            return [...usuarios, ...responseMapeada];
+                        }
+                    });
+                }
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
             });
+    }
 
-            return {
-              ...usuario,
-              isFavorito: true,
-            };
-          });
+    return (
+        <>
+            <Header />
+            <div className={styles['container']}>
+                <div className={styles['content']}>
+                    <h1 className={styles["favoritos__titulo"]}>Favoritos</h1>
+                    <div className={styles["favoritos__header"]}>
+                        <h2 className={styles["favoritos__quantidade-desenvolvedores"]}>
+                            {totalUsuarios?.current} desenvolvedores na lista de favoritos
+                        </h2>
+                        <SelectOrdernar
+                            valueOrdenar={valueOrdenar}
+                            setValueOrdenar={setValueOrdenar}
+                        />
+                    </div>
 
-          setConjuntoSkill(conjuntoSkillResponse);
-          setUsuarios(responseMapeada);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+                    <div className={styles['usuarios']}>
+                        {
+                            isLoading ?
+                                Array.from(Array(8).keys()).map((item) => (
+                                    <CardPerfilSketon key={item}/>
+                                ))
+                                :
+                                usuarios.map((usuario) => (
+                                    <CardPerfil
+                                        key={usuario.id}
+                                        usuario={usuario}
+                                        isTelaFavoritos={true}
+                                        setUsuarios={setUsuarios}
+                                        usuariosSelecionados={usuariosSelecionados}
+                                        setUsuariosSelecionados={setUsuariosSelecionados}
+                                    />
+                                ))
+                        }
+                    </div>
+                    {
+                        usuarios.length < totalUsuarios.current &&
+                        <div className={styles['usuarios__ver-mais']}>
+                            <Button style={{ color: 'var(--color-cinza)', marginBottom: '32px', fontWeight: '500' }} onClick={handleVerMais}>Ver mais</Button>
+                        </div>
+                    }
+                </div>
+            </div>
+            {
+                usuariosSelecionados.length > 1 &&
+                <CompararUsuarios
+                    usuariosSelecionados={usuariosSelecionados}
+                    setUsuariosSelecionados={setUsuariosSelecionados}
+                    conjuntoSkill={conjuntoSkill}
+                    showComparacao={showComparacao}
+                    setComparacao={setComparacao}
+                />
+            }
 
-  return (
-    <>
-      <Header />
-      <div className={styles["container"]}>
-        <div className={styles["content"]}>
-          <h1 className={styles["favoritos__titulo"]}>Favoritos</h1>
-          <div className={styles["favoritos__header"]}>
-            <h2 className={styles["favoritos__quantidade-desenvolvedores"]}>
-              {usuarios.length} desenvolvedores na lista de favoritos
-            </h2>
-            <SelectOrdernar
-              valueOrdenar={valueOrdenar}
-              setValueOrdenar={setValueOrdenar}
-            />
-          </div>
-
-          <div className={styles["usuarios"]}>
-            {usuarios.map((usuario) => (
-              <CardPerfil
-                key={usuario.id}
-                usuario={usuario}
-                isTelaFavoritos={true}
-                setUsuarios={setUsuarios}
-                usuariosSelecionados={usuariosSelecionados}
-                setUsuariosSelecionados={setUsuariosSelecionados}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-      {usuariosSelecionados.length > 1 && (
-        <CompararUsuarios
-          usuariosSelecionados={usuariosSelecionados}
-          setUsuariosSelecionados={setUsuariosSelecionados}
-          conjuntoSkill={conjuntoSkill}
-          showComparacao={showComparacao}
-          setComparacao={setComparacao}
-        />
-      )}
-    </>
-  );
+        </>
+    )
 };
 
 export default Favoritos;
